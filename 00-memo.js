2f780eea7ac3d8b29597595cf903b00a7a19a735
smile <smilemet@gmail.com>
2022-01-31 12:43:40 +0900

    auto modified

--- 00-memo.js
+++ 00-memo.js
@@ -250,25 +250,29 @@
 
 
 
-console.group("1차 배열의 탐색");
-    const data = [4, 5, 2, 1, 3];
-    for (const item of data) {
-        console.log(item);
-    }
-console.groupEnd();
+// console.group("1차 배열의 탐색");
+//     const data = [4, 5, 2, 1, 3];
+//     for (const item of data) {
+//         console.log(item);
+//     }
+// console.groupEnd();
 
 
-console.group("2차 배열의 탐색");
-    const myArr = [
-        [1, 2, 3],
-        [4, 5, 6]
-    ];
+// console.group("2차 배열의 탐색");
+//     const myArr = [
+//         [1, 2, 3],
+//         [4, 5, 6]
+//     ];
 
-    for(let item of myArr) {
-        console.log(item);
+//     for(let item of myArr) {
+//         console.log(item);
         
-        for(let sub of item) {
-            console.log(sub);
-        }
-    }
-console.groupEnd();
+//         for(let sub of item) {
+//             console.log(sub);
+//         }
+//     }
+// console.groupEnd();
+
+
+// test 
+
