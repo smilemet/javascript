9336eb2547d883eb168cedec34da354fb702abbe
smilemet <smilemet@gmail.com>
2022-02-03 15:01:38 +0900

    auto modified

--- 00-기타실습/00-memo.js
+++ 00-기타실습/00-memo.js
@@ -48,31 +48,144 @@
 //     }
 // }
 
-// console.log("1주일간의 전체 급여= %d원", money);
+// // console.log("1주일간의 전체 급여= %d원", money);
 
 
 
-// 4번 문제
-let price = [38000, 20000, 17900, 17900];
-let qty = [6, 4, 3, 5];
-let money = 0;
+// // 4번 문제
+// let price = [38000, 20000, 17900, 17900];
+// let qty = [6, 4, 3, 5];
+// let money = 0;
+
+// for (let i = 0; i < price.length; i++) {
+//     money += price[i]*qty[i];
+// }
+
+// console.log("전체 결제 금액: %d원", money);
+
+
+
+// // 5번 문제
+// let moneyArray = [
+//     price[0]*qty[0],
+//     price[1]*qty[1],
+//     price[2]*qty[2],
+//     price[3]*qty[3],
+// ]
+// let max = moneyArray[0];
+
+// for (let i = 1; i < moneyArray.length; i++) {
+//     if (max < moneyArray[i]) {
+//         max = moneyArray[i];
+//     }
+// }
+
+// console.log("가장 높은 상품금액: %d원", max);
+
+
+
+// // 6번 문제
+// const FREE_DELI = 80000;
+// let count = 0;
+
+// for (let i = 0; i < moneyArray.length; i++) {
+//     if (moneyArray[i] >= FREE_DELI) {
+//         count += 1;
+//     }
+// }
+
+// console.log("무료배송 항목: %d건", count);
+
+
+
+// // 7번 문제
+// let price = [209000, 109000, 119000, 109000, 94000];
+// console.log("상품가격 --> %d", price);
+
+// for (let i = 0; i < price.length - 1; i++) {
+//     for (let j = i + 1; j < price.length; j++) {
+//         if (price[i] > price[j]) {
+//             let tmp = price[i];
+//             price[i] = price[j];
+//             price[j] = tmp;
+//         }
+//     }
+// }
+
+// console.log("낮은가격순 --> %d", price);
+
+
+
+// // 8번 문제
+// let arr = [5, 3, 2, 8, 9]
+// console.log("before --> %s", arr);
+
+// for (let i = 0; i < parseInt(arr.length/2); i++) {
+//     let j = arr.length - i - 1;
+//     let tmp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = tmp;
+// }
+
+// console.log("after --> %s", arr);
+
+
+
+// // 문제 9
+// let student = ["둘리", "도우너", "또치", "희동"];
+// let grade = [
+//     [78, 89, 96],
+//     [62, 77, 67],
+//     [54, 90, 80],
+//     [100, 99, 98],
+// ]
+
+// let sum = 0, avg = 0;
+
+// for (let i = 0; i < student.length; i++) {
+//     for (let j = 0; j < grade[i].length; j++) {
+//         sum += grade[i][j];
+//         avg = sum/grade[i].length;
+//     }
+//     console.log("%s 총점: %d점, 평균: %d점", student[i], sum, avg.toFixed(2));
+//     sum = 0;
+//     avg = 0;
+// }
+
+
+
+// 문제 10
+let student = ["둘리", "도우너", "또치", "희동"];
+let grade = [
+    [78, 89, 96],
+    [62, 77, 67],
+    [54, 90, 80],
+    [100, 99, 98],
+]
+
+let sum = 0, avg = 0;
+let totalAvg = 0;
+
+for (let i = 0; i < student.length; i++) {
+    for (let j = 0; j < grade[i].length; j++) {
+        sum += grade[i][j];
+        avg = sum/grade[i].length;
+    }
+    console.log("%s 총점: %d점, 평균: %d점", student[i], sum, avg.toFixed(2));
+    totalAvg += avg;
+    sum = 0;
+    avg = 0;
+}
+console.log("반 평균 = %d점", totalAvg/student.length);
+
+
+
+// 문제 11
+let price = [500, 320, 100, 120, 92, 30]
+let item = [291, 586, 460, 558, 18, 72]
+
+let sum = 0
 
 for (let i = 0; i < price.length; i++) {
-    money += price[i]*qty[i];
+    
 }
-
-console.log("전체 결제 금액: %d원", money);
-
-
-
-// 5번 문제
-for(let i = 0; i < price.length; i++) {
-
-
-}
-
-
-let moneyArray = []
-    for(let i = 0; i < price.length; i++) {
-        console.log(price[i]*qty[i]);
-    }
